6b003f24324f9b12ba93f47ef8ec6fe4203f4c76
bahare-ghasemi-ma <bahareghasemi.ma@gmail.com>
2025-01-05 15:22:13 +0330

    title add to model because it was deleted

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -12,6 +12,7 @@ class User(models.Model):
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  
+    title = models.CharField(max_length=200)
     content = models.TextField() 
     created_at = models.DateTimeField(auto_now_add=True) 
     updated_at = models.DateTimeField(auto_now=True)  
